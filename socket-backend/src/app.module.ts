4896653d9b06284c21728af1b337fdc2477ac310
laguta-danil <laguta.d.s@gmail.com>
2022-01-25 19:18:56 +0200

    add_db

--- socket-backend/src/app.module.ts
+++ socket-backend/src/app.module.ts
@@ -5,7 +5,7 @@ import { ChatGateway } from './chat.gateway';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [MongooseModule.forRoot('mongodb+srv://Danil:111999111@cluster.bwcx1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService, ChatGateway],
 })
